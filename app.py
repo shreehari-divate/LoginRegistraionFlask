451161e6e0e0c6c937ac7d70c971a7180373de67
shreehari-divate <shreeharidivate03@gmail.com>
2024-06-04 10:49:17 +0530

    added git ignore and made some changes in working

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask, redirect, render_template, request, url_for
 from flask_login import LoginManager, UserMixin, login_user
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///logreg.db'
-app.config['SECRET_KEY'] = 'asdf123'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 db=SQLAlchemy(app)
 migrate=Migrate(app,db)
 
@@ -53,12 +57,23 @@ def register():
         name = request.form.get('name')
         email = request.form.get('email')
         password = request.form.get('password')
+
         if name and email and password:
+            user_exists = User.query.filter_by(email=email).first()
+            if user_exists:
+                msg = "Email is already registered"
+                return render_template("register.html",msg=msg)
+            
+            hash_password = generate_password_hash(password)
+
             user = User(name=name, email=email, password=password)
             db.session.add(user)
             db.session.commit()
             message="Successfully Regitered"
             return render_template("register.html",message=message)
+        else:
+            msg = 'Please fill out all the fields'
+            return render_template('register.html',msg=msg)
     return render_template("register.html")
 
 @app.route("/index")
